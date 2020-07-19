bc690d30da32fc8d184fd13066db6ca045e3131f
Hugo Passos <hugocastelanibp@gmail.com>
2020-07-19 00:58:48 -0300

    Create `CurrencyFormat`, `CurrencyISOCode` and `Separator`

---
+++ packages/core/lib/src/models/currency_format.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'currency_format.freezed.dart';
+
+/// The currency format setting for the budget.
+@freezed
+abstract class CurrencyFormat with _$CurrencyFormat {
+  factory CurrencyFormat({
+    required CurrencyISOCode isoCode,
+    required int decDigits,
+    required Separator decSep,
+    required bool symbolFirst,
+    required Separator groupSeparator,
+    required String currencySymbol,
+    required bool displaySymbol,
+  }) = _CurrencyFormat;
+}
+
+/// The supported currency ISO codes.
+enum CurrencyISOCode { eur, brl, usd }
+
+/// The currency separators for decimals and groups.
+enum Separator { comma, period }

d5aff6cbfe83d757ef5aaa0c22676492f14e1c56
Hugo Passos <hugocastelanibp@gmail.com>
2020-07-19 22:02:45 -0300

    Create payee

---
+++ packages/core/lib/src/payee/models/payee.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../account/models/account.dart';
+
+part 'payee.freezed.dart';
+
+@freezed
+abstract class Payee with _$Payee {
+  const factory Payee({
+    @required String name,
+    @required bool isDeleted,
+    @required Option<AccountId> transferAccount,
+  }) = _Payee;
+}
+
+@freezed
+abstract class PayeeId with _$PayeeId {
+  const factory PayeeId(String raw) = _PayeeId;
+}
